61c2769327b5fbd73082c6351613cad73fe5cdf7
ly <1095305853@qq.com>
2022-03-28 21:46:07 +0800

    commit

---
+++ spring-cloud-openfign-consumer/src/main/java/com/liuyi/springcloud/openfign/consumer/controller/EchoService.java
@@ -0,0 +1,27 @@
+package com.liuyi.springcloud.openfign.consumer.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "service-provider", fallback = EchoServiceFallback.class, configuration = FeignConfiguration.class)
+public interface EchoService {
+    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    String echo(@PathVariable("str") String str);
+}
+
+class FeignConfiguration {
+    @Bean
+    public EchoServiceFallback echoServiceFallback() {
+        return new EchoServiceFallback();
+    }
+}
+
+class EchoServiceFallback implements EchoService {
+    @Override
+    public String echo(@PathVariable("str") String str) {
+        return "echo fallback";
+    }
+}
